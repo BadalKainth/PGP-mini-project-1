f21c5ec561278370741fb302eec5cf6e2ffae3f9
badalkainth <badalkainth06@gmail.com>
2023-06-04 22:20:04 +0530

    test

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -23,7 +23,7 @@ function Footer() {
             </div>
           </div>
           <div>
-            <p>hello@dweep.io</p>
+            <p>hello@dweep.io </p>
           </div>
         </div>
       </div>
